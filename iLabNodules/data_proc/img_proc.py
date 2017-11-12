41109fbdad581e589f1705083f72b2b4f3451495
lyq <lyq211003@gmail.com>
2017-11-12 17:24:01 +0800

    .

--- iLabNodules/data_proc/img_proc.py
+++ iLabNodules/data_proc/img_proc.py
@@ -8,15 +8,32 @@ import os
 
 def load_img(request):
     res = {}
-    mhd_file = os.path.join(BASE_DIR, "data/1.3.6.1.4.1.14519.5.2.1.6279.6001.100225287222365663678666836860.mhd")
-    save_path = os.path.join(BASE_DIR, "index/static/image")
-    cnt = load.load_img(mhd_file, save_path)
-    res['max_cnt'] = cnt
+    file_name = request.GET['src']
+    mhd_file = os.path.join(BASE_DIR, "data/" + str(file_name))
+    if os.path.isfile(mhd_file):
+        save_path = os.path.join(BASE_DIR, "index/static/image")
+        cnt = load.load_img(mhd_file, save_path)
+        res['max_cnt'] = cnt
+        print(request.GET['src'])
 
     return JsonResponse(res)
 
 
 def load_nodules(request):
-    res = {}
-
+    file_name = request.GET['src']
+    mhd_file = os.path.join(BASE_DIR, "data/" + str(file_name))
+    res = {'nodules': [
+        {'x': 1, 'y': 2, 'z': 10},
+        {'x': 2, 'y': 4, 'z': 30},
+        {'x': 1, 'y': 2, 'z': 76},
+        {'x': 2, 'y': 4, 'z': 25},
+        {'x': 1, 'y': 2, 'z': 98},
+        {'x': 2, 'y': 4, 'z': 26},
+        {'x': 1, 'y': 2, 'z': 13},
+        {'x': 2, 'y': 4, 'z': 39},
+        {'x': 1, 'y': 2, 'z': 11},
+        {'x': 2, 'y': 4, 'z': 70},
+        {'x': 1, 'y': 2, 'z': 80},
+        {'x': 2, 'y': 4, 'z': 100}
+    ]}
     return JsonResponse(res)
